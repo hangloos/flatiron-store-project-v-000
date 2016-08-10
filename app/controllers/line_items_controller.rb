0a49689f32d4a6f80fa6d0626d4ef86d94721376
hangloos <ericloos00@gmail.com>
2016-08-10 08:53:21 -0400

    Tests Passing, before refactoring

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -6,14 +6,15 @@ class LineItemsController < ApplicationController
     @item = Item.find(params[:item_id])
     @user = current_user
     @cart = @user.current_cart
-    
+
       if @cart.nil?
         @cart = Cart.new
         @cart.user = @user
         @cart.save
         @user.current_cart = @cart
         @user.save
-        @cart.add_item(@item.id).save
+        new_item = @cart.add_item(@item.id)
+        new_item.save
         redirect_to @cart
       else
         new_item = @cart.add_item(@item.id)
